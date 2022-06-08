93f2eb5d1474e9643fb2773902b577b2c5bd2156
sireesha <msireesha30031999@gmail.com>
2022-06-08 22:32:14 +0530

    IPL DashBoard -> addded fetch query to get list of IPL teams details from backend

--- src/components/IPLDashBoard/index.js
+++ src/components/IPLDashBoard/index.js
@@ -4,48 +4,66 @@ import './index.css'
 import IPLTeam from '../IPLTeam'
 
 
-const iplTeamList = [{
-    id:"RCB",
-    name:"Royal Challengers",
-    teamImageUrl:"https://assets.ccbp.in/frontend/react-js/rcb-logo-img.png"
-},
-{
-    id:"KKR",
-    name:"Royal Challengers testing",
-    teamImageUrl:"https://assets.ccbp.in/frontend/react-js/kkr-logo-img.png"
-},
-{
-    id:"RCB",
-    name:"Royal Challengers",
-    teamImageUrl:"https://assets.ccbp.in/frontend/react-js/rcb-logo-img.png"
-},
-{
-    id:"KKR",
-    name:"Royal Challengers testing",
-    teamImageUrl:"https://assets.ccbp.in/frontend/react-js/kkr-logo-img.png"
-},{
-    id:"RCB",
-    name:"Royal Challengers",
-    teamImageUrl:"https://assets.ccbp.in/frontend/react-js/rcb-logo-img.png"
-},
+// const iplTeamList = [{
+//     id:"RCB",
+//     name:"Royal Challengers",
+//     teamImageUrl:"https://assets.ccbp.in/frontend/react-js/rcb-logo-img.png"
+// },
+// {
+//     id:"KKR",
+//     name:"Royal Challengers testing",
+//     teamImageUrl:"https://assets.ccbp.in/frontend/react-js/kkr-logo-img.png"
+// },
+// {
+//     id:"RCB",
+//     name:"Royal Challengers",
+//     teamImageUrl:"https://assets.ccbp.in/frontend/react-js/rcb-logo-img.png"
+// },
+// {
+//     id:"KKR",
+//     name:"Royal Challengers testing",
+//     teamImageUrl:"https://assets.ccbp.in/frontend/react-js/kkr-logo-img.png"
+// },{
+//     id:"RCB",
+//     name:"Royal Challengers",
+//     teamImageUrl:"https://assets.ccbp.in/frontend/react-js/rcb-logo-img.png"
+// },
 
-]
+// ]
 
 
 class IPLDashBoard extends Component{
+    state = {iplTeamList:[]}
     
+    componentDidMount(){
+        this.getIPLTeamslist()
+    }
+
+    getIPLTeamslist = async () =>{
+         const response = await fetch("https://apis.ccbp.in/ipl")
+        const data = await response.json()
+        // console.log(data.teams)
+        const updatedData = data.teams.map(eachTeam =>({
+            id:eachTeam.id,
+            name:eachTeam.name,
+            teamImageUrl:eachTeam.team_image_url
+        }))
+        this.setState({iplTeamList:updatedData})
+    }
+
 
     render(){
+        const {iplTeamList} = this.state
+
         return(
-            <div className='app-container'>
+            <div className='app-bg-container'>
                 <div className='app-content-container'>
                     <div className='logo-heading-container'>
                         <img src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png" alt="ipl-logo" className='logo-img'/>
                         <h1 className='main-heading'>IPL DashBoard</h1>
                     </div>
                     <ul className='team-card-container'>
-                        {iplTeamList.map(eachTeam =>(<IPLTeam teamDetails={eachTeam}/>))}
-                       
+                        {iplTeamList.map(eachTeam =>(<IPLTeam teamDetails={eachTeam} key={eachTeam.id}/>))}  
                     </ul>
                 </div>
             </div>
